ee85e5ac52a5f0dc074117f295345009789e8953
whao <wang_yi@163.com>
2022-03-22 18:38:17 +0800

    修改

--- src/App.vue
+++ src/App.vue
@@ -11,16 +11,14 @@ setVH()
 
 const store = new ReplStore({
   serializedState: location.hash.slice(1),
-  defaultVueRuntimeURL: import.meta.env.PROD
-    ? `${location.origin}/vue.runtime.esm-browser.js`
-    : `${location.origin}/src/vue-dev-proxy`
+  defaultVueRuntimeURL: import.meta.env.PROD ? undefined : `${location.origin}/src/vue-dev-proxy`,
 })
 
 // enable experimental features
 const sfcOptions = {
   script: {
-    reactivityTransform: true
-  }
+    reactivityTransform: true,
+  },
 }
 
 // persist state
@@ -43,8 +41,8 @@ watchEffect(() => history.replaceState({}, '', store.serialize()))
 <style>
 body {
   font-size: 13px;
-  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-    Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans',
+    'Helvetica Neue', sans-serif;
   margin: 0;
   --base: #444;
   --nav-height: 50px;
